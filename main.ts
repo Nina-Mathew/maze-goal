9adf5135ea9bf5562d48f97c49551b59ccca1cbc
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-11-03 10:47:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+// Created by: Nina Mathew
+// Created on 11/3/2020
+// Shows the number of times a goal is made.
+let score = 0
+basic.showNumber(score)
+basic.pause(100)
 basic.forever(function () {
 	
 })
