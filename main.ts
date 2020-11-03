87ac656c5575f3210c0d183fce6ffd08ecff2f76
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-11-03 11:15:31 -0500

    added goal things

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 // Created by: Nina Mathew
 // Created on 11/3/2020
 // Shows the number of times a goal is made.
+let goal = 0
 let score = 0
 basic.showNumber(score)
 basic.pause(100)
 basic.forever(function () {
-	
+    goal = pins.digitalReadPin(DigitalPin.P0)
+    if (goal == 1) {
+        score = score + 1
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showNumber(score)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
