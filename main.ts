b1baba69f93769de785c41768f143e97717b5b04
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-11-12 12:51:41 -0500

    added buzzer music

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
  * Created by: Nina Mathew
  */
 let goal = 0
+music.playMelody("C5 C5 E E B B C C ", 120)
 let score = 0
 basic.showNumber(score)
 basic.pause(100)
@@ -10,12 +11,11 @@ basic.pause(100)
  */
 // Created on 11/1/20
 basic.forever(function () {
-    goal = pins.digitalReadPin(DigitalPin.P0)
+    goal = pins.digitalReadPin(DigitalPin.P1)
     if (goal == 1) {
         score = score + 1
-        pins.digitalWritePin(DigitalPin.P2, 1)
         basic.showNumber(score)
+        music.playMelody("C5 A B E F A B F ", 120)
         basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P2, 0)
     }
 })
